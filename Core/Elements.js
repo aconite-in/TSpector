e0ee3e8e078a6273955b393d670110964131159e
aconite <gaikwad.abhishek.m@gmail.com>
2019-04-23 14:31:23 +0530

    remove gitignore files

--- Core/Elements.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=Elements.js.map
--- Core/WebElements/Label.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const BaseElement_1 = require("./BaseElement");
-class Label extends BaseElement_1.BaseElement {
-}
-exports.Label = Label;
-//# sourceMappingURL=Label.js.map
