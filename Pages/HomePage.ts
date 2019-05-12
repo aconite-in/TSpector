d3bc61a6f4b90ebcdc0ec9a64dedbfa11bc2d7b0
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-05-12 14:40:29 +0530

    Added Logout button

--- Pages/HomePage.ts
+++ Pages/HomePage.ts
@@ -1,10 +1,13 @@
 import { BasePage } from "../Core/BasePage";
 import { HtmlTable } from "../Core/WebElements/HtmlTable";
+import { LoginPage } from "./LoginPage";
+import { LinkLabel } from "../Core/WebElements/LinkLabel";
 
 
 export class HomePage extends BasePage {
 
     public RecentJobs: HtmlTable = new HtmlTable("id", "main_content_pneGridUsers", "#main_content_pneGridUsers_lbForward", undefined, "class");
+    public LogoutLink: LinkLabel = new LinkLabel("css", "#mainContainer > table > tbody > tr > td:nth-child(5) > a")
 
     constructor() {
         super("", "//*[@id='main_pageHeader_lblHeader1']");
