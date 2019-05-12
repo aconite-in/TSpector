14115d78ad5f62ec22103440fc0e619965729cf2
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-05-12 14:31:18 +0530

    Added wait before clicking

--- Core/WebElements/Button.ts
+++ Core/WebElements/Button.ts
@@ -1,11 +1,17 @@
 import { BaseElement } from "./BaseElement";
 import { Logger, LogLevel } from "../DataAccess/Logger";
+import { browser, protractor } from "protractor";
 
 export class Button extends BaseElement {
 
-    public async click() {
-        await this.get().click().then(
-            () => Logger.log(LogLevel.INFO, `Button, Clicked button with locator ${this.locatorType} with value ${this.locatorValue}`)
-        );
+    public async click(failOnError: boolean = true) {
+        await browser.wait(async () => { return await this.get().isPresent(); }, 10000).then(async () => {
+            var EC = protractor.ExpectedConditions;
+            var elm = await this.get();
+            await browser.wait(async () => { return await EC.elementToBeClickable(elm); }, 10000).then(async () => {
+                await elm.click();
+                Logger.log(LogLevel.INFO, `Button: Clicked Button with ${this.locatorType} =  ${this.locatorValue}`);
+            }, (err) => { Logger.log(failOnError ? LogLevel.ERROR : LogLevel.WARN, `Button: Failed to click Button with ${this.locatorType} =  ${this.locatorValue}`) });
+        }, (err) => { Logger.log(failOnError ? LogLevel.ERROR : LogLevel.WARN, `Button: Failed to click Button with ${this.locatorType} =  ${this.locatorValue}`) });
     }
 }
