0beff34c0aab5d3259be6a97b65fd649bfc82361
Gaikwad <gaikwad.abhishek.m@gmail.com>
2019-05-12 21:32:34 +0530

    Changed logic of wait to handle corner cases

--- Core/WebElements/Button.ts
+++ Core/WebElements/Button.ts
@@ -5,11 +5,10 @@ import { browser, protractor } from "protractor";
 export class Button extends BaseElement {
 
     public async click(failOnError: boolean = true) {
-        await browser.wait(async () => { return await this.get().isPresent(); }, 10000).then(async () => {
+        await browser.wait(async () => { return await this.get().isDisplayed(); }, 10000).then(async () => {
             var EC = protractor.ExpectedConditions;
-            var elm = await this.get();
-            await browser.wait(async () => { return await EC.elementToBeClickable(elm); }, 10000).then(async () => {
-                await elm.click();
+            await browser.wait(async () => { return await EC.elementToBeClickable(this.get()); }, 10000).then(async () => {
+                await this.get().click();
                 Logger.log(LogLevel.INFO, `Button: Clicked Button with ${this.locatorType} =  ${this.locatorValue}`);
             }, (err) => { Logger.log(failOnError ? LogLevel.ERROR : LogLevel.WARN, `Button: Failed to click Button with ${this.locatorType} =  ${this.locatorValue}`) });
         }, (err) => { Logger.log(failOnError ? LogLevel.ERROR : LogLevel.WARN, `Button: Failed to click Button with ${this.locatorType} =  ${this.locatorValue}`) });
