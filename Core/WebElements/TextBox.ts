d501c44591907a202616b3d1177e7af092a3080a
Gaikwad <Abhishek.Gaikwad@FISGLOBAL.COM>
2019-05-14 19:26:49 +0530

    Removed commented code

--- Core/WebElements/TextBox.ts
+++ Core/WebElements/TextBox.ts
@@ -4,19 +4,13 @@ import { browser } from "protractor";
 
 export class TextBox extends BaseElement {
 
-    // public async type(inputText: string) {
-    //     await this.get().sendKeys(inputText).then(
-    //         () => Logger.log(LogLevel.INFO, `TextBox, entered "${inputText}"locator "${this.locatorType}" with value "${this.locatorValue}"`)
-    //     ).catch(() => Logger.log(LogLevel.ERROR, `TextBox, unable to entered "${inputText}"locator "${this.locatorType}" with value "${this.locatorValue}"`));
-    // }  
-
     public async type(inputText: string) {
         await browser.wait(() => { return this.get().isPresent(); }, 10000).then(async () => {
             await browser.wait(async () => { return await this.get().isEnabled(); }, 10000).then(async () => {
                 await this.get().clear();
                 await this.get().sendKeys(inputText);
                 Logger.log(LogLevel.INFO, `TextBox: Typed ${inputText} on element with ${this.locatorType} =  ${this.locatorValue}`);
-            }, (err) => { Logger.log(LogLevel.ERROR, `TextBox: Failed to Type ${inputText} on element with ${this.locatorType} =  ${this.locatorValue}`); }); //throw new Error('Error occurred!'); this will fail the whole script
+            }, (err) => { Logger.log(LogLevel.ERROR, `TextBox: Failed to Type ${inputText} on element with ${this.locatorType} =  ${this.locatorValue}`); });
         }, (err) => { Logger.log(LogLevel.ERROR, `TextBox: Failed to Type ${inputText} on element with ${this.locatorType} =  ${this.locatorValue}`); });
     }
 }
